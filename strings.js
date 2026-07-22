3a42dc919ca258896d5d618d50abaab030a5819b
Kumari <2503624@cognizant.com>
2026-07-22 20:54:35 +0530

    string definition

--- strings.js
+++ strings.js
@@ -1,4 +1,8 @@
-let greeting = 'Hello "Adity"'
-console.log(greeting)
-greeting =  `Hello "Adity" How was your day`
+// let greeting = 'Hello "Adity"'
+// console.log(greeting)
+// greeting =  `Hello "Adity" How was your day`
+// console.log(greeting)
+
+let name = "Adity"
+greeting = `Hello ${name}  How was your day`
 console.log(greeting)
